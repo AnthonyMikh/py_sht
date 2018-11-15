cff83cc8026e5424912fcd938a5e0840af705cbb
AnthonyMikh <anthony.mikh@yandex.ru>
2018-11-15 19:50:36 +0300

    Simplify code
    Get rid of gen_* functions and implement a simpler and more performant password generator

--- src/pass_gen/main.py
+++ src/pass_gen/main.py
@@ -1,40 +1,33 @@
 import argparse as arp
-import random as rnd
+from random import choice
 
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALPHABET_LO = "abcdefghijklmnopqrstuvwxyz"
 ALPHABET_UP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 SYMBOLS = "~!?@#$%^&*()"
-
-def gen_letter_lo() -> str:
-    return rnd.choice(ALPHABET_LO)
-
-def gen_letter_up() -> str:
-    return rnd.choice(ALPHABET_UP)
-
-def gen_symbol() -> str:
-    return rnd.choice(SYMBOLS)
-
-def gen_digit() -> str:
-    return rnd.choice("0123456789")
+DIGITS = "0123456789"
 
 def gen_pass(length: int, *, symbols: bool = True, digits: bool = True) -> str:
     if length <= 0:
         raise ValueError("Cannot generate password "\
                          f"of non-positive length {length}")
 
-    gens = [gen_letter_lo, gen_letter_up]
+    required = [choice(ALPHABET_LO), choice(ALPHABET_UP)]
+    charset = ALPHABET
     if symbols:
-        gens.append(gen_symbol)
+        required.append(choice(SYMBOLS))
+        charset += SYMBOLS
     if digits:
-        gens.append(gen_digit)
-    base = len(gens)
+        required.append(choice(DIGITS))
+        charset += DIGITS
+
+    base = len(required)
     if base > length:
         raise ValueError("Cannot generate password with all required "\
                          f"categories with length {length}")
 
-    igens = range(len(gens))
-    additional = "".join(gens[rnd.choice(igens)]() for _ in range(length - base))
-    return "".join(f() for f in gens) + additional
+    additional = "".join(choice(charset) for _ in range(length - base))
+    return "".join(required) + additional
 
 if __name__ == "__main__":
     print(gen_pass(8))
